54f9a6068833b49925d521304b037097f1d64b7e
Peter Li <li.peter7122@gmail.com>
2021-05-04 21:15:42 -0400

    Final code

---
+++ src/finalMnist.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import keras
+from keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print("Training data shape: ", x_train.shape) # (60000, 28, 28) -- 60000 images, each 28x28 pixels
+print("Test data shape", x_test.shape) # (10000, 28, 28) -- 10000 images, each 28x28
+
+# Flatten the images
+image_vector_size = 28*28
+x_train = x_train.reshape(x_train.shape[0], image_vector_size)
+x_test = x_test.reshape(x_test.shape[0], image_vector_size)
+
+
+##############################
+print("Training label shape: ", y_train.shape) # (60000,) -- 60000 numbers (all 0-9)
+print("First 5 training labels: ", y_train[:5]) # [5, 0, 4, 1, 9]
+
+# Convert to "one-hot" vectors using the to_categorical function
+num_classes = 10
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+print("First 5 training lables as one-hot encoded vectors:\n", y_train[:5])
+
+from keras.layers import Dense
+from keras.models import Sequential
+
+image_size = 784 # 28*28
+
+model = Sequential()
+
+# The input layer requires the special input_shape parameter which should match
+# the shape of our training data.
+model.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
+model.add(Dense(units=num_classes, activation='softmax'))
+model.summary()
+
+import matplotlib.pyplot as plt
+
+model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=False, validation_split=.1)
+loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
+print(history.history.keys())
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['training', 'validation'], loc='best')
+plt.show()
+
+print(f'Test loss: {loss:.3}')
+print(f'Test accuracy: {accuracy:.3}')
